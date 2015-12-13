348470cfb4924a41a625d66a5555caa204596eb8
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-12-13 14:34:50 +0100

    Ember-try: test against multiple versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,6 +6,74 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-1.13.0',
+      dependencies: { 'ember': '1.13.0' }
+    },
+    {
+      name: 'ember-1.13.1',
+      dependencies: { 'ember': '1.13.1' }
+    },
+    {
+      name: 'ember-1.13.2',
+      dependencies: { 'ember': '1.13.2' }
+    },
+    {
+      name: 'ember-1.13.3',
+      dependencies: { 'ember': '1.13.3' }
+    },
+    {
+      name: 'ember-1.13.4',
+      dependencies: { 'ember': '1.13.4' }
+    },
+    {
+      name: 'ember-1.13.5',
+      dependencies: { 'ember': '1.13.5' }
+    },
+    {
+      name: 'ember-1.13.6',
+      dependencies: { 'ember': '1.13.6' }
+    },
+    {
+      name: 'ember-1.13.7',
+      dependencies: { 'ember': '1.13.7' }
+    },
+    {
+      name: 'ember-1.13.8',
+      dependencies: { 'ember': '1.13.8' }
+    },
+    {
+      name: 'ember-1.13.9',
+      dependencies: { 'ember': '1.13.9' }
+    },
+    {
+      name: 'ember-1.13.10',
+      dependencies: { 'ember': '1.13.10' }
+    },
+    {
+      name: 'ember-1.13.11',
+      dependencies: { 'ember': '1.13.11' }
+    },
+    {
+      name: 'ember-2.0.0',
+      dependencies: { 'ember': '2.0.0' }
+    },
+    {
+      name: 'ember-2.0.1',
+      dependencies: { 'ember': '2.0.1' }
+    },
+    {
+      name: 'ember-2.0.2',
+      dependencies: { 'ember': '2.0.2' }
+    },
+    {
+      name: 'ember-2.1.0',
+      dependencies: { 'ember': '2.1.0' }
+    },
+    {
+      name: 'ember-2.1.1',
+      dependencies: { 'ember': '2.1.1' }
+    },
+    {
       name: 'ember-release',
       dependencies: {
         'ember': 'components/ember#release'
