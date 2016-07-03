3a35c0aaaa5e026ddb513c02b78d7a71a8c017f1
Baptiste Meurant <baptiste@meurant.io>
2016-07-03 19:56:36 +0200

    Update to ember-cli 2.6.2

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,7 @@ module.exports = function(defaults) {
   if (app.env !== 'production') { app.import('bower_components/ember/ember-template-compiler.js', { type: 'vendor' }); }
 
   /*
-    This build file specifes the options for the dummy test app of this
+    This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
