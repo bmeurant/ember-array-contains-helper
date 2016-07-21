f763387d528662cde24cf0914e923ae418dd5a0c
Baptiste Meurant <baptiste@meurant.io>
2016-07-21 16:10:41 +0200

    docs(yuidocs): setup yuidoc & theme, make documentation compliant

--- addon/helpers/array-contains.js
+++ addon/helpers/array-contains.js
@@ -1,4 +1,8 @@
-// Note: exported packages are defined in ember.js/vendor/ember/shims.js
+/**
+ * @module ember-array-contains-helper
+ */
+
+// Note: exported modules are defined in ember-cli/ember-cli-shims/blob/master/app-shims.js
 import { isNone } from 'ember-utils';
 import { A as emberArray, isEmberArray as isArray } from 'ember-array/utils';
 import { assert } from 'ember-metal/utils';
@@ -7,14 +11,14 @@ import set from 'ember-metal/set';
 import { addObserver, removeObserver } from 'ember-metal/observer';
 
 /**
- * Helper proving a way to test the presence of an item in an array.
+ * Helper providing a way to test the presence of an item in an array.
  *
  * Depending on the given parameters, the test is made on
- * - the presence of a literal:
+ * - the presence of a **literal**:
  *    ``{{array-contains array 'foo'}}``
- * - the presence of the object itself:
+ * - the presence of **the object itself**:
  *    ``{{array-contains array object}}``
- * - the presence of an object containing a specific property with a specific value:
+ * - the presence of **an object containing a specific property with a specific value**:
  *    ``{{array-contains array 'value' property='title'}}``
  *
  * Note that null or undefined are considered acceptable and equivalent values for 'value' parameter (resolve both to null)
@@ -22,13 +26,13 @@ import { addObserver, removeObserver } from 'ember-metal/observer';
  * This helper could be used standalone or, more often, combined with the ``if`` helper:
  *
  * ```html
- * {{if (array-contains array 'value' property='title') 'something' 'something else'}}
+ *   {{if (array-contains array 'value' property='title') 'something' 'something else'}}
  * ```
  *
  * This addon installs observers on the provided array to listen any external change made on it. It includes any addition/removal
  * of an item and, if a property is specified, any change of the property of any array element.
  *
- * **extends** [Ember.Helper](https://github.com/emberjs/ember.js/blob/f96aecd212fe96b9560c8832b7bbb4988d6670d0/packages/ember-htmlbars/lib/helper.js#L48)
+ * @extends {Ember.Helper}
  *
  * @public
  * @class ArrayContainsHelper
@@ -38,20 +42,17 @@ export default Helper.extend({
   /**
    * Test if an array contains an object, eventually based on one of this object property / value
    *
-   * Returns true if:
-   * - the array in ``params[0]`` contains the object value in ``params[1]``
-   * - the array in ``params[0]`` contains an object holding a property named ``hash.property`` with
-   * value equals to ``params[1]``
-   *
-   * Returns false otherwise and if the array in ``params[0]`` is null or undefined
-   *
-   * **extends** [Ember.Helper.compute](https://github.com/emberjs/ember.js/blob/f96aecd212fe96b9560c8832b7bbb4988d6670d0/packages/ember-htmlbars/lib/helper.js#L82)
-   *
    * @method compute
    * @param {Array} params array and value to test. Array can be null, undefined or valid Array.
    * value could be a literal or an object
    * @param {Object} hash named arguments accepted by this helper (``property``)
-   * @throws {Ember.Error} [(Ember.Error)](https://github.com/emberjs/ember.js/blob/27862a18b8773e57c52dbc7141b1f92a505f16ff/packages/ember-metal/lib/error.js#L22) if params is null or not an array or if the given array (from ``params[0]``) is
+   * @return {Boolean}
+   * - true if:
+   *   - the array in ``params[0]`` contains the object value in ``params[1]``
+   *   - the array in ``params[0]`` contains an object holding a property named ``hash.property`` with value equals to ``params[1]``
+   * - false otherwise and if the array in ``params[0]`` is null or undefined
+   *
+   * @throws {Ember.Error} if params is null or not an array or if the given array (from ``params[0]``) is
    * not null and not an array.
    */
   compute(params, hash) {
