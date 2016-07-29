97dd9bc3b0268e6f5eb2b6e9e23b83d14eface00
Baptiste Meurant <baptiste@meurant.io>
2016-07-29 21:51:48 +0200

    chore(ember-try): test until ember 2.7.0 with Travis MATRIX
    Rollback to travis MATRIX for each ember-try scenario because of Travis CI timeouts. Update ember-try version compatibility from 1.13.1 to 2.7.0

---
+++ config/ember-try.js
@@ -0,0 +1,67 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'default',
+      dependencies: { }
+    },
+    {
+      name: 'ember-1.13',
+      dependencies: { 'ember': '1.13.11' }
+    },
+    {
+      name: 'ember-2.0',
+      dependencies: { 'ember': '2.0.3' }
+    },
+    {
+      name: 'ember-2.1',
+      dependencies: { 'ember': '2.1.2' }
+    },
+    {
+      name: 'ember-2.2',
+      dependencies: { 'ember': '2.2.2' }
+    },
+    {
+      name: 'ember-2.3',
+      dependencies: { 'ember': '2.3.2' }
+    },
+    {
+      name: 'ember-2.4',
+      dependencies: { 'ember': '2.4.6' }
+    },
+    {
+      name: 'ember-2.5',
+      dependencies: { 'ember': '2.5.1' }
+    },
+    {
+      name: 'ember-2.6',
+      dependencies: { 'ember': '2.6.2' }
+    },
+    {
+      name: 'ember-release',
+      dependencies: {
+        'ember': 'components/ember#release'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'ember-beta',
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    },
+    {
+      name: 'ember-canary',
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
+      }
+    }
+  ]
+};
