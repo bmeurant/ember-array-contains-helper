79897afbe9262bdba64769725da3d1c5e71fda56
Baptiste Meurant <baptiste@meurant.io>
2016-08-02 11:52:29 +0200

    Deploy gh-pages from commit 27ff101aeead2c56d08aba426c6cae24b32c29fe

---
+++ docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ArrayContainsHelper"
+    ],
+    "modules": [
+        "ember-array-contains-helper"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-array-contains-helper",
+            "name": "ember-array-contains-helper"
+        }
+    ],
+    "elements": []
+} };
+});
