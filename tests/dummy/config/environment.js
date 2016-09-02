948b3e01e60433a7a6efe85da24fcc3e17709e20
Baptiste Meurant <baptiste@meurant.io>
2016-09-02 14:42:47 +0200

    chore(coverage): switch from blanket to ember-cli-code-coverage

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -50,6 +50,9 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV['ember-cli-mirage'] = {
+      enabled: false
+    }
   }
 
   if (environment === 'production') {
