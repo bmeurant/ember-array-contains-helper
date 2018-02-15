1508ce4a7f0601068a522967479c3dca225650c4
Baptiste Meurant <baptiste.meurant@worldline.com>
2018-02-15 09:57:57 +0000

    chore(ci): add tests for ember-2.12-lts

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,6 +11,14 @@ module.exports = function() {
     return {
       scenarios: [
         {
+          name: 'ember-lts-2.12',
+          npm: {
+            devDependencies: {
+              'ember-source': '~2.12.0'
+            }
+          }
+        },
+        {
           name: 'ember-lts-2.16',
           npm: {
             devDependencies: {
