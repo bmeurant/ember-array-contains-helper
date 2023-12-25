4ef8f044f674cd39da8afb32925da12cd9727b4d
Miguel Andrade <jmandrade0@gmail.com>
2023-12-25 22:24:55 +0000

    give up dummy app 2

--- server/index.js
+++
@@ -1,8 +0,0 @@
-/* global module */
-/* eslint-disable ember/use-ember-get-and-set */
-
-module.exports = function (app) {
-  app.get('/docs', function (req, res) {
-    res.redirect('/docs/index.html');
-  });
-};
